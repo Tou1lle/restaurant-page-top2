5dd3ee69b387d181997ef4479d16ff9dd6a6a1e6
Tuan Ha <hatuan@email.cz>
2025-05-20 23:50:53 +0200

    Add hardcoded html page for menu, create files
    The menu page is divided into parts with div
    containers. Each div container has a header and
    a list of items.
    
    The list items will be expanded. Each items will
    be like a card with price etc.
    
    Added css and js files for future work.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import "./styles/common-styles.css";
 import * as homePage from "./scripts_pages/home_page"
 
 console.log("Webpack is working!");
-homePage.createHomePage();
+//homePage.createHomePage();
